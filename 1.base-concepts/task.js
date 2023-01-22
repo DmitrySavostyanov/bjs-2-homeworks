3d1711b10571593c68b91e30fd062a0564c05015
Dmitry Savostyanov <dokken671@gmail.com>
2023-01-22 16:28:50 +0700

    Реализовано первое задание к ДЗ№1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,17 @@
-"use strict"
+"use strict";
+//Задача №1
 function solveEquation(a, b, c) {
+
   let arr = [];
-  
-  return arr;
+  let discriminant = b**2-4*a*c;
+  if (discriminant === 0) {
+    arr.push(- b / (2 * a));
+  }
+ else if (discriminant > 0) {
+  arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+  arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  } 
+  return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
