8cfcd7902b100ef3cb8da1fc8f12211cb21268e2
Dmitry Savostyanov <dokken671@gmail.com>
2023-01-29 13:39:30 +0700

    Реализовано 1 и 2 задание к ДЗ№1 - fix Change_3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,8 @@
 //задача №1
 "use stict";
-//let a = Number(prompt("Введите а: "));//2
-//let b = Number(promt("Введите b: "));//4
-//let c = Number(prompt("Введите с: "));//6
-//solveEquation(a, b, c) //вызов функции
-
 function solveEquation(a, b, c) {
-
   let arr = new Array();
-  let d = (b * b) - (4 * a * c);// d = 4 * 4 - 4 * 2 * 6 = -32
+  let d = (b * b) - (4 * a * c);
   alert("Дискиминант = " + d);
   if (d < 0) {
     alert("Квадратное уравнение не имеет корней");
@@ -30,25 +24,21 @@ function solveEquation(a, b, c) {
   }
 }
 
-
 //задача №2
-
 let percent = Number(prompt("Введите процентную ставку в %"));//процентная ставка
 let contribution = Number(prompt("Введите сумму первого взноса в руб."));//сумма перв. взноса
 let amount = Number(prompt("Введите общую стоимость ипотечного кредита в руб."));// общая стоимость
 let countMonths = Number(prompt("Введите срок ипотеки (кол-во месяцев)"));
 calculateTotalMortgage(percent, contribution, amount, countMonths) //вызов функции
 
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-
   // Контроль корректности введенных данных. 
-  if (Number.isNaN(percent) || percent < 0) {
-    return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
-  } else if (Number.isNaN(contribution) || contribution < 0) {
-    return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
-  } else if (Number.isNaN(amount) || amount < 0) {
-    return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
+  if (isNaN(percent)) {
+    return false;
+  } else if (isNaN(contribution)) {
+    return false;
+  } else if (isNaN(amount)) {
+    return false;
   }
   let per = percent / 100 / 12; //процентная ставка по условию
   let s = amount - contribution; //тело кредита
@@ -56,5 +46,3 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let totalAmount = +(pay * countMonths).toFixed(2);//сумма, которую придётся заплатить клиенту.
   return totalAmount;
 }
-
-
