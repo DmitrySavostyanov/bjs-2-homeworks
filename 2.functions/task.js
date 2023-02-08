d37bf26c3d81f1e4cfba80aa30b1907f01c773b8
Dmitry Savostyanov <dokken671@gmail.com>
2023-02-08 23:24:14 +0700

    HW_2_task_1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,28 @@
+"use strict"
+//Задача №1
 function getArrayParams(...arr) {
+  let min, max, sum;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] > max){
+      max = arr[i];
+    } 
+    if (arr[i] < min){
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  alert(sum);
+  let avg = +(sum/arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
+/*
+
+//Задача №2
 function summElementsWorker(...arr) {
 
 }
@@ -19,6 +39,9 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
+
+//Задача №3
 function makeWork (arrOfArr, func) {
 
 }
+*/
