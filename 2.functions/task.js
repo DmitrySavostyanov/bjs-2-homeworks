50ee16caf99779676c33522e23fcfc7c9153a118
Dmitry Savostyanov <dokken671@gmail.com>
2023-02-08 23:55:47 +0700

    HW_2_task_2or3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -20,28 +20,66 @@ function getArrayParams(...arr) {
   return { min: min, max: max, avg: avg };
 }
 
-/*****
-
 //Задача №2
 function summElementsWorker(...arr) {
-
+ if (arr.length) {
+    return arr.reduce((accumulator, current) => accumulator + current, 0); 
+  } else {
+    return 0;
+  }
 }
 
 function differenceMaxMinWorker(...arr) {
-
+if (arr.length) {
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    return max - min; 
+  } else {
+    return 0;
+  }
 }
 
 function differenceEvenOddWorker(...arr) {
+ if (arr.length) {
 
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
+
+    for (let num of arr) {
+      if ( num % 2 === 0) sumEvenElement += num;
+      else sumOddElement += num;
+    }
+    return sumEvenElement - sumOddElement; 
+  } else {
+    return 0;
+  }
 }
 
 function averageEvenElementsWorker(...arr) {
+ if (arr.length) {
+
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
+  
+    for (let num of arr) {
+      if ( num % 2 === 0) {
+        sumEvenElement += num;
+        countEvenElement++;
+      }
+    }
+    return sumEvenElement / countEvenElement; 
+  } else {
+    return 0;
+  }
 
 }
-
 
 //Задача №3
 function makeWork (arrOfArr, func) {
+let maxWorkerResult = -Infinity;
+  for (let arr of arrOfArr) {
+    if (func(...arr) > maxWorkerResult) maxWorkerResult = func(...arr);
+  } 
 
+  return maxWorkerResult;
 }
-*/
